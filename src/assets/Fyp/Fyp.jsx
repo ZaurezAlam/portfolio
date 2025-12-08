57ad69955b5d5abc82a41b3a3459861606f466cf
ZaurezAlam <137296532+ZaurezAlam@users.noreply.github.com>
2025-12-08 15:05:26 +0500

    Update GitHub link in Fyp.jsx

--- src/assets/Fyp/Fyp.jsx
+++ src/assets/Fyp/Fyp.jsx
@@ -17,7 +17,7 @@ export default function Projects() {
       title: "PortFolio",
       description: "A modern React portfolio showcasing my skills, projects, and experience Designed with clean UI, responsive layout, and smooth user experience.",
       image: "https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?q=80&w=1955&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      github: "https://github.com/yourrepo",
+      github: "https://github.com/ZaurezAlam/portfolio",
       category: "other"
     },
     {
