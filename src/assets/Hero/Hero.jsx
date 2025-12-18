c3db7ad96c56165c4dda48a29853dbd6e0fd93e5
ZaurezAlam <zaurezalam@gmail.com>
2025-12-18 17:52:02 +0500

    Refactor Hero component text and layout for improved clarity and structure

--- src/assets/Hero/Hero.jsx
+++ src/assets/Hero/Hero.jsx
@@ -6,18 +6,25 @@ export const Hero = () => {
     <div className="flex flex-col md:flex-row justify-center items-center  text-white font-medium mt-12  ">
       
       {/* Text */}
-      <div className="pr-19">
-        <p className="mb-6 leading-relaxed mr-12">
-         Aspiring React.js Intern passionate about building<br/> responsive, modern interfaces using React, JavaScript, and<br/>
-          CSS. Focused on creating clean UI and learning industry best practices.<br/>          
-        </p>
-        <a
-          href="https://github.com/ZaurezAlam"
-          className="inline-block bg-orange-500 text-white font-semibold px-6 py-3 rounded-lg hover:bg-orange-600 transition"
-        >
-          Learn More
-        </a>
-      </div>
+      <div className="pr-40">
+  <h2 className="text-orange-500 font-serif font-bold text-xl mb-4">
+    About Me
+  </h2>
+
+  <p className="mb-6 leading-relaxed mr-12">
+    Aspiring React.js Intern passionate about building<br />
+    responsive and modern user interfaces using React,<br />
+    JavaScript, and CSS while learning best practices.
+  </p>
+
+  <a
+    href="https://github.com/ZaurezAlam"
+    className="inline-block bg-orange-500 text-white font-semibold px-6 py-3 rounded-lg hover:bg-orange-600 transition"
+  >
+    Learn More
+  </a>
+</div>
+
 
       {/* Image */}
       <div className="w-60 h-60  ml-24 -translate-y-1  ">
